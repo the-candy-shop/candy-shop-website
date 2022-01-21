9cdeaadf9cc21b6c7b953b64ba25717f902f57d1
Georges Biaux <georges.biaux@gadz.org>
2022-01-21 09:50:36 +0100

    Integration

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ function App() {
           <img
             alt="logo"
             src={logo}
-            style={{ borderRadius: "50%", width: "150px" }}
+            style={{ borderRadius: "50%", height: "150px" }}
           />
         </Box>
         <Box
-          marginTop="128px"
+          marginTop="100px"
           fontSize="18px"
           lineHeight="27px"
           sx={{
@@ -27,62 +27,68 @@ function App() {
             },
           }}
         >
-          <h1>Our vision</h1>
-          <p>
-            We are here to push forward what's possible on-chain with The Candy
-            Shop, our NFT innovation lab.
-          </p>
-          <p>
-            <Link href="https://chaindreamers.xyz/" target="_blank">
-              Chain Dreamer
-            </Link>{" "}
-            is the vehicle for what we consider our first innovation: putting
-            vector illustration on-chain with our own custom encoding system
-            explained in{" "}
-            <Link
-              href="https://chaindreamers.xyz/on-chain-storage"
-              target="_blank"
-            >
-              this article
-            </Link>
-            .
-          </p>
-          <p>
-            Each new innovation by the Candy Shop will be a new set of drugs.
-          </p>
-          <p>
-            We'll never do any whitelist. Instead, we'll determine who can mint
-            based on specific NFT ownership. Of course, owning a Chain Dreamer
-            will give you a special access to all our future collections and
-            drugs.
-          </p>
-          <p>
-            We chose{" "}
-            <Link href="https://www.chainrunners.xyz/" target="_blank">
-              Chain Runner
-            </Link>{" "}
-            as our first partner project for obvious reasons: on-chain, great
-            narrative, builder community...
-          </p>
-          <p>We will develop new projects following 3 golden rules:</p>
-          <ul>
-            <li>Innovation mechanism (tech & minting experience)</li>
-            <li>Expanding what we consider the best NFT franchises</li>
-            <li>Narrative-driven</li>
-          </ul>
-          <h3 style={{ marginTop: "48px" }}>Next steps</h3>
-          <p>
-            In the short term, our next improvement will be the Chain Dreamer
-            rendering function. Today, while all information is stored on-chain,
-            we didn't manage to keep the rendering function below the 50m gas
-            limit for free rendering.
-          </p>
-          <p>We'll work on this right after the minting is done.</p>
-          <p>
-            We'll use a part of the money to build a beautiful 3D Metaverse
-            version of the Candy Shop.
-          </p>
-          <h3 style={{ marginTop: "48px" }}>Next project</h3>
+          <h1>Introducing The Candy Shop</h1>
+          <h2 style={{ marginTop: "32px" }}>
+            A new, narrative-driven NFT lab that will push forward what's
+            technically possible on-chain
+          </h2>
+          <Box sx={{ marginTop: "48px" }}>
+            <p>
+              We are here to push forward what's possible on-chain with The
+              Candy Shop, our NFT innovation lab.
+            </p>
+            <p>
+              <Link href="https://chaindreamers.xyz/" target="_blank">
+                Chain Dreamer
+              </Link>{" "}
+              is the vehicle for what we consider our first innovation: putting
+              vector illustration on-chain with our own custom encoding system
+              explained in{" "}
+              <Link
+                href="https://chaindreamers.xyz/on-chain-storage"
+                target="_blank"
+              >
+                this article
+              </Link>
+              .
+            </p>
+            <p>
+              Each new innovation by the Candy Shop will be a new set of drugs.
+            </p>
+            <p>
+              We'll never do any whitelist. Instead, we'll determine who can
+              mint based on specific NFT ownership. Of course, owning a Chain
+              Dreamer will give you a special access to all our future
+              collections and drugs.
+            </p>
+            <p>
+              We chose{" "}
+              <Link href="https://www.chainrunners.xyz/" target="_blank">
+                Chain Runner
+              </Link>{" "}
+              as our first partner project for obvious reasons: on-chain, great
+              narrative, builder community...
+            </p>
+            <p>We will develop new projects following 3 golden rules:</p>
+            <ul>
+              <li>Innovation mechanism (tech & minting experience)</li>
+              <li>Expanding what we consider the best NFT franchises</li>
+              <li>Narrative-driven</li>
+            </ul>
+            <h2 style={{ marginTop: "48px" }}>Next steps</h2>
+            <p>
+              In the short term, our next improvement will be the Chain Dreamer
+              rendering function. Today, while all information is stored
+              on-chain, we didn't manage to keep the rendering function below
+              the 50m gas limit for free rendering.
+            </p>
+            <p>We'll work on this right after the minting is done.</p>
+            <p>
+              We'll use a part of the money to build a beautiful 3D Metaverse
+              version of the Candy Shop.
+            </p>
+          </Box>
+          <h2 style={{ marginTop: "48px" }}>Next project</h2>
           <p>
             There are high probabilities we'll stay for a while in Mega-City but
             we'll eventually expand to any Metaverse world.
